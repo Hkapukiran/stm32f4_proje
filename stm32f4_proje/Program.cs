2a2f0f40a31594ea58fed0e3a789ab7ac9befe0d
Hasan Kapukiran <h.kapukiran@gmail.com>
2016-06-17 02:52:17 +0300

    dll_paket_islemleri_eklendi

--- stm32f4_proje/Program.cs
+++ stm32f4_proje/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace stm32f4_proje
 {
@@ -12,9 +13,18 @@ namespace stm32f4_proje
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+        [DllImport("PaketIslemleri.dll")] 
+        public static extern void  UINT32_ayir  (byte[] p_veriler_u8,   ref byte  p_paket_sayaci_u8, ref uint  d_gelen_veri_u32);
+        
         static void Main()
         {
             //baslangic
+
+            uint a = 254565;
+            byte sayac = 0;
+            byte[] veriler = new byte[256];
+
+            UINT32_ayir(veriler,ref  sayac,ref  a);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
